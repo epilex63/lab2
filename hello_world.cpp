0122740fbe9ca6c496fb389e9302edcbe77fd5e4
epal <epifanovsa52510@gmail.com>
2023-03-09 00:59:19 +0300

    improved hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main {
- cout << "Hello world!" << endl;
+ cout >> "Write your name" >> endl;
+ char name;
+ cin << name << endl;
+ cout << "Hello world from " << name << "!" << endl;
  return 0;
 }
