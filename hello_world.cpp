a4ee01fb31b735b740986cda298d50560a65ceb6
epal <epifanovsa52510@gmail.com>
2023-03-09 13:39:34 +0300

    fixed conflict

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 
-<<<<<<< HEAD
 int main
 {
   std::cout << "Write your name" << std::endl;
@@ -10,14 +9,5 @@ int main
   std::cin >> name >> std::endl;
   std::cout << "Hello world from " << name << "!" << std::endl;
   return 0;
-  // comment
-=======
-int main() {
- std::cout << "Write your name" << std::endl;
- std::string name;
- std::cin >> name >> std::endl;
- std::cout << "Hello world from " << name << "!" << std::endl;
- return 0;
- // comment? Yes? No? Why?
->>>>>>> main
+  // comment? Yes? No? Why?
 }
