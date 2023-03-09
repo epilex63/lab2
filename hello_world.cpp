db72bc3334e51340e826305d12538766f3730c6f
epal <epifanovsa52510@gmail.com>
2023-03-09 11:58:09 +0300

    used clang format

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 
-int main {
- std::cout << "Write your name" << std::endl;
- std::string name;
- std::cin >> name >> std::endl;
- std::cout << "Hello world from " << name << "!" << std::endl;
- return 0;
- // comment
+int main
+{
+  std::cout << "Write your name" << std::endl;
+  std::string name;
+  std::cin >> name >> std::endl;
+  std::cout << "Hello world from " << name << "!" << std::endl;
+  return 0;
+  // comment
 }
