5a2ad47fbf0b94469199cec3193d587a10834cd3
Александр Епифанов <112869736+epilex63@users.noreply.github.com>
2023-03-09 12:07:41 +0300

    Update hello_world.cpp
    added additional comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <string>
 
-int main {
+int main() {
  std::cout << "Write your name" << std::endl;
  std::string name;
  std::cin >> name >> std::endl;
  std::cout << "Hello world from " << name << "!" << std::endl;
  return 0;
- // comment
+ // comment? Yes? No? Why?
 }
